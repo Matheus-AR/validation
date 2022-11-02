3bd1a40f2b6102d526a4d1749f0154fb6dd0ba1f
HC4MAC29 <hc4mac29@HC4MAC29.local>
2022-11-01 21:32:56 -0300

    Falta algumas partes do código

--- controllers/contatoController.js
+++ controllers/contatoController.js
@@ -3,15 +3,11 @@ const Contato = require('../models/contatoModel');
 
 async function criar(req, res) {
     const contato = new Contato(req.body);
-    const erros = [];
-    await contato.save().catch(error => {
-        if(error.errors['nome']) erros.push(error.errors['nome'].message);
-        if(error.errors['fone']) erros.push(error.errors['fone'].message);
-    });
-    if (erros.length > 0) {
-        return res.status(422).json(erros);
-    }
-    return res.status(201).json(contato);
+    await contato.save()
+        .then(doc => { return res.status(201).json(doc)})
+        .catch(error => {
+            
+        });
 }
 
 async function listar(req, res) {
@@ -30,12 +26,17 @@ async function consultar(req, res) {
 }
 
 async function atualizar(req, res) {
-    await Contato.updateOne({_id: ObjectId(req.params.id)}, req.body)
+    await Contato.findOneAndUpdate({_id: ObjectId(req.params.id)}, req.body, { runValidators: true })
         .then(contato => {
             if(contato) return res.status(204).end();
             else return res.status(404).json('Contato não encontrado')
         })
-        .catch(error => { return res.status(500).json(error)});
+        .catch(error => {
+            const msgErro = {};
+            Object.values(error.errors).forEach(({ properties })=>{
+                msgErro(properties.path) = properties.message;
+            });
+        });
 }
 
 async function remover(req, res) {
--- models/contatoModel.js
+++ models/contatoModel.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const contatoSchema = new mongoose.Schema({
     nome: {
         type: String,
-        required: [true, 'Nome é obrigatório']
+        required: [true, 'Nome é obrigatório'],
+        trim: true,
+        minLength: [3, 'Nome deve conter pelo menos 3 caracteres']
     },
     fone: {
         type: String,
@@ -14,4 +16,3 @@ const contatoSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Contato', contatoSchema);
-// 
